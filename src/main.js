106e5ae35ce14f2fdd590f7711e71c2b18ca556f
imnirfn <s3ns3xy@yahoo.com>
2020-09-16 09:14:52 +0800

    pg ent

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue';
 import VueCompositionAPI from '@vue/composition-api';
 import hooks from '@u3u/vue-hooks';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import '../node_modules/bulma/css/bulma.css';
 import '../node_modules/bulma-pageloader/dist/css/bulma-pageloader.min.css';
 
+library.add(faUserSecret);
+Vue.component('fa', FontAwesomeIcon);
 Vue.use(hooks);
 Vue.use(VueCompositionAPI);
 
--- src/views/Enterprise.vue
+++ src/views/Enterprise.vue
@@ -10,29 +10,28 @@
             </h2>
             <h1 class="column">test</h1>
         </section>
-
-        <section class="hero is-light" style="width: 100%">
-            <div class="container">
-                <h1 class="title mt-5">Market insights</h1>
-                <div class="subtitle mt-5 rows">
-                    <h2 class="row">Gain a quick glimpse into your
-                        market standings or investigate your
-                        overall<br/> performance. With market-wide views, our data allows you to:
-                    </h2>
-                    <div class="row mt-5">
-                        <span class="icon mr-3">
-                            <i class="fas fa-home">dads</i>
-                        </span>
-                        <span class="icon mr-3">
-                            <i class="fas fa-home">dads</i>
-                        </span>
-                        <span class="icon">
-                            <i class="fas fa-home">dads</i>
-                        </span>
-                    </div>
+        </div>
+    </section>
+    <section class="hero is-light pt-0" style="width: 100%">
+        <div class="container" style="width: 100%">
+            <h1 class="title mt-5">Market insights</h1>
+            <div class="subtitle mt-5 rows">
+                <h2 class="row">Gain a quick glimpse into your
+                    market standings or investigate your
+                    overall<br/> performance. With market-wide views, our data allows you to:
+                </h2>
+                <div class="row mt-5">
+                    <span class="icon mr-3">
+                        <fa class="is-medium" icon="user-secret"/>
+                    </span>
+                    <span class="icon mr-3">
+                        <i class="fas fa-home">dads</i>
+                    </span>
+                    <span class="icon">
+                        <i class="fas fa-home">dads</i>
+                    </span>
                 </div>
             </div>
-        </section>
         </div>
     </section>
   </div>
@@ -42,20 +41,9 @@ import { ref } from '@vue/composition-api';
 
 export default {
   setup() {
-    const histories = ref([]);
-    const API_URL = 'http://ec2-54-255-128-152.ap-southeast-1.compute.amazonaws.com:5001/api/v1/dynamo/url-model';
-
-    async function getHistory() {
-      const response = await fetch(API_URL);
-      const json = await response.json();
-      console.log('here', json.Items);
-      histories.value = json.Items;
-    }
-
-    getHistory();
-
+    const test = ref[''];
     return {
-      histories,
+      test,
     };
   },
 };
