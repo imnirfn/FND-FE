4ab0631a1f176f3d00ecf6acd705314a164e424a
Dolpheyn <farissufyan99@gmail.com>
2020-09-16 11:27:25 +0800

    Auth header

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Journarlist.vue'),
+    component: () => import(/* webpackChunkName: "about" */ '../views/Journalist.vue'),
   },
   {
     path: '/scan',
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,25 @@
 import axios from 'axios';
+import store from '../store';
+import { getToken } from '../utils/auth';
 
 // http://ec2-54-255-128-152.ap-southeast-1.compute.amazonaws.com:5001
 const api = axios.create({
   baseURL: 'http://ec2-54-255-128-152.ap-southeast-1.compute.amazonaws.com:5001/api/v1/',
 });
 
+api.interceptors.request.use(
+  // eslint-disable-next-line arrow-parens
+  config => {
+    if (store.getters.token) {
+      // eslint-disable-next-line no-param-reassign
+      config.headers.Authorization = `Bearer ${getToken()}`;
+    }
+    return config;
+  },
+  // eslint-disable-next-line arrow-parens
+  error => {
+    console.log(error);
+    Promise.reject(error);
+  },
+);
 export default api;
